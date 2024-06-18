85f8540e0b26b6a870c0fdd6cd15a7be88e15285
ReyNeill <reyneill@rentan.ai>
2024-06-18 20:46:25 +0100

    finishing touches

---
+++ worker.js
@@ -0,0 +1,7 @@
+import { WebWorkerMLCEngineHandler } from "https://cdn.jsdelivr.net/npm/@mlc-ai/web-llm@0.2.46/+esm"
+
+const handler = new WebWorkerMLCEngineHandler()
+
+self.onmessage = (msg) => {
+    handler.onmessage(msg)
+}
